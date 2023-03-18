4304d971cf1b8c579112488aae2163c35edd7b70
Kei Webber <keiwebber3@gmail.com>
2023-03-18 07:42:58 -0600

    save progress back button

--- main_android.py
+++ main_android.py
@@ -51,10 +51,11 @@ Builder.load_string("""
 
 <ListeningPage>
     name: "listening_page"
+    id: listen_page
 
 <SpeakingPage>
     name: "speaking_page"    
-
+    id: speak_page
 """)
 
 # Declare both screens
@@ -66,16 +67,15 @@ class LearningMainPage(Screen):
 
 class ListeningPage(Screen):
     def on_pre_enter(self, *args):
-       lbp = ListeningBox()
-       lbp.build()
-       self.add_widget(lbp)
+       self.lbp = ListeningBox()
+       self.lbp.build()
+       self.add_widget(self.lbp)
 
 class SpeakingPage(Screen):
     def on_pre_enter(self, *args):
-       sbp = SpeakingBox()
-       sbp.build()
-       self.add_widget(sbp)
-       
+       self.sbp = SpeakingBox()
+       self.sbp.build()
+       self.add_widget(self.sbp)
 
 class SearchPage(Screen):
     def on_pre_enter(self, *args):
@@ -100,7 +100,14 @@ class TestApp(App):
 
     def hook_keyboard(self, window, key, *largs):
         if key in [27,1001]:
-            self.sm.current = "menu"
+            if self.sm.current == "listening_page":
+                self.sm.get_screen("listening_page").lbp.SaveResults()
+                self.sm.current = "learn_page"
+            elif self.sm.current == "speaking_page":
+                self.sm.get_screen("speaking_page").sbp.SaveResults()
+                self.sm.current = "learn_page"
+            else:
+                self.sm.current = "menu"
             return True
 
 
